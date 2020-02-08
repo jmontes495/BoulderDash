d98f6b325e43aaa8d9f1eb9044c158554fca7b05
Juliana Montes <jmontes@jamcity.com>
2020-02-08 09:18:40 -0500

    Cleaned up the project

---
+++ BoulderDash/Assets/Scripts/UI/LevelUI.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelUI : MonoBehaviour
+{
+    private float timeRemaining;
+    private bool timeIsRUnning;
+
+    [SerializeField] private TextMeshProUGUI timer;
+    [SerializeField] private TextMeshProUGUI gemsCounter;
+    [SerializeField] private TextMeshProUGUI score;
+    [SerializeField] private TextMeshProUGUI lifes;
+
+    private void Start()
+    {
+        if (timer != null)
+            timer.text = "<sprite=0>000";
+
+        if (gemsCounter != null)
+            gemsCounter.text = "<sprite=1>0/0";
+
+        GameStats.GemsUpdated += UpdateGemsCounter;
+        GameStats.TimeUpdated += UpdateTimer;
+        GameStats.LifesUpdated += UpdateLifeCounter;
+        GameStats.ValuesInitialized += SetAllValues;
+    }
+
+    public void SetTime(float time)
+    {
+        timeRemaining = time;
+    }
+
+    public void StartTimer()
+    {
+        timeIsRUnning = true;
+    }
+
+    private void UpdateTimer()
+    {
+        if (timer != null)
+        {
+            string color = GameController.Instance.GameStats.TimeRemaining > 10 ? "<color=white>" : "<color=red>";
+            timer.text = color + "<sprite=0>" + GameController.Instance.GameStats.TimeRemaining + "</color>";
+        }
+    }
+
+    private void UpdateGemsCounter()
+    {
+        if (gemsCounter != null)
+        {
+            string color = GameController.Instance.GameStats.GemsCollected < GameController.Instance.GameStats.GemsNeeded ? "<color=#00FFFF>" : "<color=white>";
+            gemsCounter.text = color + "<sprite=1>" + GameController.Instance.GameStats.GemsCollected + "/" + GameController.Instance.GameStats.GemsNeeded + "</color>";
+        }
+
+        if (score != null)
+            score.text = GameController.Instance.GameStats.Score.ToString();
+    }
+
+    private void UpdateLifeCounter()
+    {
+        if (lifes != null)
+        {
+            string lifesRemaining = "";
+            for (int i = 0; i < GameController.Instance.GameStats.Lifes; i++)
+                lifesRemaining += "<sprite=2>";
+
+            lifes.text = lifesRemaining;
+        }
+    }
+
+    private void SetAllValues()
+    {
+        UpdateGemsCounter();
+        UpdateLifeCounter();
+    }
+
+    private void OnDestroy()
+    {
+        GameStats.GemsUpdated -= UpdateGemsCounter;
+        GameStats.TimeUpdated -= UpdateTimer;
+        GameStats.LifesUpdated -= UpdateLifeCounter;
+        GameStats.ValuesInitialized -= SetAllValues;
+    }
+
+    public void ShowElements(bool state)
+    {
+        timer.gameObject.SetActive(state);
+        score.gameObject.SetActive(state);
+        gemsCounter.gameObject.SetActive(state);
+        lifes.gameObject.SetActive(state);
+    }
+}
---
+++ BoulderDash/Assets/Scripts/UI/SpriteConfig.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteConfig : ScriptableObject
+{
+    [SerializeField]  private Sprite character1;
+    [SerializeField]  private Sprite character2;
+    [SerializeField]  private Sprite characterDead;
+    [SerializeField]  private Sprite[] exit;
+    [SerializeField]  private Sprite boulder;
+    [SerializeField]  private Sprite dirt;
+    [SerializeField]  private Sprite brick;
+    [SerializeField]  private Sprite[] gem;
+
+    public Sprite Character1
+    {
+        get { return character1; }
+    }
+
+    public Sprite Character2
+    {
+        get { return character2; }
+    }
+
+    public Sprite CharacterDead
+    {
+        get { return characterDead; }
+    }
+
+    public Sprite[] Exit
+    {
+        get { return exit; }
+    }
+
+    public Sprite Boulder
+    {
+        get { return boulder; }
+    }
+
+    public Sprite Dirt
+    {
+        get { return dirt; }
+    }
+
+    public Sprite Brick
+    {
+        get { return brick; }
+    }
+
+    public Sprite[] Gem
+    {
+        get { return gem; }
+    }
+}
